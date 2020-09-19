5fbcdfbc21aa0e247755124f5691199030b5f211
Hexagon Six <hexagon6@fet.li>
2020-09-20 00:53:22 +0200

    Add voice feature

--- App.js
+++ App.js
@@ -1,21 +1,70 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import React from "react";
+import { StyleSheet, Text, View, Button, AppRegistry } from "react-native";
+import Voice from "react-native-voice";
+export default class VoiceNative extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      recognized: "",
+      started: "",
+      results: [],
+    };
+    Voice.onSpeechStart = this.onSpeechStart.bind(this);
+    Voice.onSpeechRecognized = this.onSpeechRecognized.bind(this);
+    Voice.onSpeechResults = this.onSpeechResults.bind(this);
+  }
+  componentWillUnmount() {
+    Voice.destroy().then(Voice.removeAllListeners);
+  }
+  onSpeechStart(e) {
+    this.setState({
+      started: "√",
+    });
+  }
+  onSpeechRecognized(e) {
+    this.setState({
+      recognized: "√",
+    });
+  }
+  onSpeechResults(e) {
+    this.setState({
+      results: e.value,
+    });
+  }
+  async _startRecognition(e) {
+    this.setState({
+      recognized: "",
+      started: "",
+      results: [],
+    });
+    try {
+      await Voice.start("en-US");
+    } catch (e) {
+      console.error(e);
+    }
+  }
+  render() {
+    return (
+      <View>
+        <Text style={styles.transcript}>Transcript</Text>
+        {this.state.results.map((result, index) => (
+          <Text style={styles.transcript}> {result}</Text>
+        ))}
+        <Button
+          style={styles.transcript}
+          onPress={this._startRecognition.bind(this)}
+          title="Start"
+        ></Button>
+      </View>
+    );
+  }
 }
-
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  transcript: {
+    textAlign: "center",
+    color: "#B0171F",
+    marginBottom: 1,
+    top: "400%",
   },
 });
+AppRegistry.registerComponent("VoiceNative", () => VoiceNative);
